dc62c0cdf915f0ab6bafdd7a3f5275ebafb6cfb6
zarir94 <shahriar@gmail.com>
2024-05-13 09:51:12 -0700

    Update

--- teraboxlinks.py
+++ teraboxlinks.py
@@ -4,8 +4,8 @@ from limiter import *
 import re
 
 def run_terabox_bot(link, proxy=None, headless=None):
-    # idn = 'urlbot-terabox'
-    # if isCompleted(1428, idn): return print('Target Completed. Function did not run')
+    idn = 'urlbot-terabox'
+    if isCompleted(1428, idn): return print('Target Completed. Function did not run')
     s=Session()
     s.proxies=dict(http=proxy, https=proxy)
     r1=s.get(link, headers={'Referer': 'https://thekisscartoon.com/'}, allow_redirects=False, stream=True)
@@ -13,7 +13,7 @@ def run_terabox_bot(link, proxy=None, headless=None):
     if loc is None:
         raise Exception('Error in teraboxlinks links. Location is None')
     print('TeraBox Links:', loc)
-    # submitOne(idn)
+    submitOne(idn)
     
 
 
