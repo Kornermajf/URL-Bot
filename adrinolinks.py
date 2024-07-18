a56cd853d554755803676d02029be89e0205200e
zarir94 <shahriar@gmail.com>
2024-07-18 07:04:07 -0700

    add xff header

--- adrinolinks.py
+++ adrinolinks.py
@@ -8,7 +8,7 @@ def run_adrino_bot(link, proxy=None, headless=None):
     # if isCompleted(1249, idn): return print('Target Completed. Function did not run')
     s=Session()
     s.proxies=dict(http=proxy, https=proxy)
-    r1=s.get(link, headers={'Referer': 'https://thekisscartoon.com/'}, allow_redirects=False)
+    r1=s.get(link, headers={'Referer': 'https://thekisscartoon.com/', 'X-Forwarded-For': generate_random_ip()}, allow_redirects=False)
     loc = r1.headers.get('Location')
     if loc is None:
         raise Exception(f'Error in adrino links. Location is None. Status: {r1.status_code}')
