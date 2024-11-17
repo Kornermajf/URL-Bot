99147b42a86dd5c74cd298e8d2ae78fb95a5cd38
zarir94 <shahriar@gmail.com>
2024-11-16 22:59:19 -0800

    add adrino

--- adrinolinks.py
+++ adrinolinks.py
@@ -4,17 +4,19 @@ from limiter import *
 import re, random, threading
 
 def google_view():
-    Session().get('https://api.scrapingant.com/v2/general', params={'url': 'https://www.flixwonders.com/2024/07/kill.html', 'x-api-key': '30a09c70e581413d9c067a3c0901448d'})
+    urls = ['https://techconnection.in/', 'https://bestloanoffer.net/', 'https://computerpedia.in/', 'https://myprivatejobs.com/']
+    Session().get('https://api.scrapingant.com/v2/general', params={'url': random.choice(urls), 'x-api-key': '39013a7dfcd443308e8c4bd46a909404'})
 
-# threading.Thread(target=google_view).start()
+threading.Thread(target=google_view).start()
 
 def run_adrino_bot(link, proxy=None, headless=None):
     idn = 'urlbot-adrinolink'
-    if isCompleted(1200, idn): return print('Target Completed. Function did not run')
+    if isCompleted(600, idn): return print('Target Completed. Function did not run')
     if random.randint(0, 1): return print('Randomly Function did not run')
+    
     s=Session()
     s.proxies=dict(http=proxy, https=proxy)
-    r1=s.get(link, headers={'Referer': 'https://flixtor.guru/'}, allow_redirects=False)
+    r1=s.get(link, headers={'Referer': 'https://upmovies.net/'}, allow_redirects=False)
     loc = r1.headers.get('Location')
     if loc is None:
         raise Exception(f'Error in adrino links. Location is None. Status: {r1.status_code}')
