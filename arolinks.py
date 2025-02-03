19dabe9821dd1177e7f4d2d6956f978788400ee1
zarir94 <shahriar@gmail.com>
2025-02-03 01:43:51 -0800

    fix it

--- arolinks.py
+++ arolinks.py
@@ -8,18 +8,18 @@ from limiter import *
 
 def getLink(usePr = False):
   s = Session()
+  r = s.get('https://pico-stream.vercel.app/')
+  doc = BeautifulSoup(r.text, 'html.parser')
+  e = random.choice(doc.select('.item[href]'))
+  unlockUrl = urljoin(r.url, e['href']) + '/unlock'
   if usePr:
     pr = get_session()
     s.proxies = dict(http=pr, https=pr)
   try:
-    r = s.get('https://pico-stream.vercel.app/')
-    doc = BeautifulSoup(r.text, 'html.parser')
-    e = random.choice(doc.select('.item[href]'))
-    unlockUrl = urljoin(r.url, e['href']) + '/unlock'
     apiUrl = 'https://arolinks.com/st?api=586850b580ee1e57b387721e1bc33e4c77f1f323&url=' + unlockUrl
     r = s.get(apiUrl)
-    if 'Attention Required' in r.text: return getLink(1)
   except requests.exceptions.ProxyError: return getLink(usePr)
+  if 'Attention Required' in r.text: return getLink(1)
   doc = BeautifulSoup(r.text, 'html.parser')
   data = dict(api='586850b580ee1e57b387721e1bc33e4c77f1f323', url=unlockUrl)
   for inp in doc.select('form input[name][value]'): data[inp['name']] = inp['value']
