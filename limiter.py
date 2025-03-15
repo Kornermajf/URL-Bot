1f0fe5b87ec58816bc35ce888c91b4504fbe5b57
zarir94 <shahriar@gmail.com>
2025-03-15 04:47:57 -0700

    fix

--- limiter.py
+++ limiter.py
@@ -1,13 +1,20 @@
 import requests as r
 import inspect
 
+class CurrentDate:
+    d = None
+    @property
+    def current(s):
+        if not s.d: s.d = get_date()
+        return s.d
+
+
+date = CurrentDate()
 
 def get_date():
     x = r.get('https://timeapi.io/api/timezone/zone?timeZone=Asia/Dhaka')
     dt = x.json().get('currentLocalTime', 'idk9910').split('T')[0]
     return dt
-
-cur_date = get_date()
 
 def isCompleted(t:int, idn:str):
     c = getRecord(idn)
@@ -15,16 +22,16 @@ def isCompleted(t:int, idn:str):
 
 
 def submitOne(idn: str):
-    q = r.get(f'https://letscountapi.com/{idn}/{cur_date}')
+    q = r.get(f'https://letscountapi.com/{idn}/{date.current}')
     if not q.json()['exists']:
-        r.post(f'https://letscountapi.com/{idn}/{cur_date}', data='{"current_value": 0}', headers={'Content-Type': 'application/json'})
-    w = r.post(f'https://letscountapi.com/{idn}/{cur_date}/increment')
+        r.post(f'https://letscountapi.com/{idn}/{date.current}', data='{"current_value": 0}', headers={'Content-Type': 'application/json'})
+    w = r.post(f'https://letscountapi.com/{idn}/{date.current}/increment')
     c = int(w.json().get('current_value', 0))
     return c
 
 
 def getRecord(idn: str):
-    q = r.get(f'https://letscountapi.com/{idn}/{cur_date}')
+    q = r.get(f'https://letscountapi.com/{idn}/{date.current}')
     c = int(q.json().get('current_value', 0))
     return c
 
