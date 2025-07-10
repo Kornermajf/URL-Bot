f12da8abc6421fd106712988265ad1f46b6f8806
zarir94 <shahriarzariracc@gmail.com>
2025-07-10 12:41:50 +0600

    add vplink

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ threading.excepthook=excepthook
 
 def main(proxy=None, **kw):
     t=[]
-    # subprocess.Popen(f'{sys.executable} {os.path.join('ShareDiskLink', 'sdl_bot.py')}'.split()).wait()
+    subprocess.Popen(f'{sys.executable} {os.path.join('VPLink', 'browser_bot.py')}'.split(), stderr=subprocess.STDOUT).wait()
     # t.append(Thread(target=lambda: run_terabox_bot(random_teraboxlinks, proxy, **kw)))
     # t.append(Thread(target=lambda: run_nano_bot(random_nanolinks, proxy, **kw)))
     # t.append(Thread(target=run_telegramlinks_bot_browser))
